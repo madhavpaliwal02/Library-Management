da8dd03c2caa68683d92bcda2148faafc8034a22
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-07-09 15:52:47 +0530

    Completed modification bean configuration

---
+++ src/main/java/com/library/entities/Admin.java
@@ -0,0 +1,7 @@
+package com.library.entities;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Admin {
+
+}
---
+++ src/main/java/com/library/entities/Student.java
@@ -0,0 +1,8 @@
+package com.library.entities;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+}
